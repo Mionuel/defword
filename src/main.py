ab0a158b86f9ea62dadd93b7ed73e7c606ce65fd
Mionuel <lifevlad2015@gmail.com>
2026-04-26 22:25:53 +0200

    feat: added translation API call and an optional target language parameter

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import click
 import requests
-from pprint import pprint
 
 dictionaryAPI = "https://freedictionaryapi.com/api/v1/entries"
+translationAPI = "https://api.mymemory.translated.net/get"
 
 @click.command()
-@click.option('-w', '--word', help='The word to define.')
-def call(word):
+@click.option('-w', '--word', required=True, help='The word to define.')
+@click.option('-o', '--output', 'output_language', help='The target (output) language.')
+# @click.option('-i', '--input', 'input_language', help='The input language.')
+def defword(word, output_language):
     word_s = sanitize_word(word)
+
     response = requests.get(f"{dictionaryAPI}/en/{word_s}")
     data = response.json()
 
@@ -15,11 +18,26 @@ def call(word):
     language_name = data['entries'][0]['language']['name']
     part_speech = data['entries'][0]['partOfSpeech']
 
-    print(f"{word_s.capitalize()} ({language_name} {part_speech}) - {first_definition}")
+    definition = f"{word_s.capitalize()} ({language_name} {part_speech}) - {first_definition}"
+
+    if not output_language:
+        print(definition)
+    else:
+        print(translate(first_definition, output_language))
 
 def sanitize_word(word):
     return word.lower().strip()
+
+def translate(text, language):
+    my_params = {
+        "q": text,
+        "langpair": f"en|{language}"
+    }
+
+    response = requests.get(translationAPI, params=my_params) 
+
+    data = response.json()   
+    return data["responseData"]["translatedText"]
   
-    
 if __name__ == '__main__':
-    call() 
+    defword() 
