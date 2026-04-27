89bb6c39e1c12c3fe0efabd8551ab9f20a98c0b1
Mionuel <lifevlad2015@gmail.com>
2026-04-27 18:12:52 +0200

    feat: updated the .gitignore file. separated definition fetching into a function. added 2 flags: --no-cache to disable cached response usage for that definition lookup; and --clear-cache to reset the stored cache

--- src/main.py
+++ src/main.py
@@ -15,31 +15,50 @@ requests_cache.install_cache('requests_cache', expire_after=86400)
 @click.argument('word')
 @click.option('-o', '--output', 'output_language', multiple=True, 
               help='The target (output) language(s). This flag can be used multiple times in order to define the word in multiple languages.')
+@click.option('--no-cache', 'no_cache', is_flag=True, help='Disables the requests caching for this call.')
+@click.option('--clear-cache', 'clear_cache', is_flag=True, help='Resets the cache file.')
 # @click.option('-h', '--history', 'history', type=int, help='This flag prints out the last 5 (default) or n definition lookups.')
 # @click.option('-i', '--input', 'input_language', help='The input language.')
-def defword(word, output_language, history):
+def defword(word, output_language, no_cache, clear_cache):
+    if clear_cache:
+        requests_cache.uninstall_cache()
+
     word_s = sanitize_word(word)
 
-    response = requests.get(f"{dictionaryAPI}/en/{word_s}")
+    definitions = []
+    if no_cache:
+        with requests_cache.disabled():
+            definitions.append(fetch_definition(word_s))
+    else:
+        definitions.append(fetch_definition(word_s))
+
+    if output_language:
+        for l in output_language:
+            translated = fetch_translation(definitions[0], "en", l)
+            definitions.append(translated)
+
+    for d in definitions:
+        print(d)
+
+# sanitezes the word by making it lowercase and trimming white spaces
+def sanitize_word(word):
+    return word.lower().strip()
+
+# fetches the english definition of an english word
+def fetch_definition(word):
+    response = requests.get(f"{dictionaryAPI}/en/{word}")
     data = response.json()
 
     first_definition = data['entries'][0]['senses'][0]['definition']
     language_name = data['entries'][0]['language']['name']
     part_speech = data['entries'][0]['partOfSpeech']
 
-    definition = f"{word_s.capitalize()} ({language_name} {part_speech}) - {first_definition}"
-
-    if not output_language:
-        print(definition)
-    else:
-        for l in output_language:
-            print(translate(first_definition, "en", l))
+    definition = f"{word.capitalize()} ({language_name} {part_speech}) - {first_definition}"
+    return definition
 
 
-def sanitize_word(word):
-    return word.lower().strip()
-
-def translate(text, in_language, out_language):
+# fetches the translation of a definition
+def fetch_translation(text, in_language, out_language):
     my_params = {
         "q": text,
         "langpair": f"{in_language}|{out_language}"
